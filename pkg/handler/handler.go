0a22fab3ffed955fabc777643e2df4adbec02dc4
HACK3R911 <afanasiev-2004@list.ru>
2025-03-21 17:12:36 +0300

    add endpoints for lists & tasks

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,1 +1,37 @@
-package internal
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct{}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.SignUp)
+		auth.POST("/sign-in", h.SignIn)
+	}
+
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			tasks := lists.Group(":id/tasks")
+			{
+				tasks.POST("/", h.createTask)
+				tasks.GET("/", h.getAllTasks)
+				tasks.GET("/:task_id", h.getTaskById)
+				tasks.PUT("/:task_id", h.updateTask)
+				tasks.DELETE("/:task_id", h.deleteTask)
+			}
+		}
+	}
+	return router
+}
