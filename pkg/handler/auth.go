f8f1a83333f2d1e5dd04b545b6c52bb953de1d4f
HACK3R911 <afanasiev-2004@list.ru>
2025-03-23 15:39:02 +0300

    add authentication with jwt

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -24,6 +24,25 @@ func (h *Handler) SignUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) SignIn(c *gin.Context) {
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func (h *Handler) SignIn(c *gin.Context) {
+	var input signInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 	"github.com/HACK3R911/go-todo-api/internal/models"
 	"github.com/HACK3R911/go-todo-api/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
-const salt = "zexsrecdhjkhulk"
+const (
+	salt       = "zexsrecdhjkhulk"
+	signingKey = "secret"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +32,23 @@ func (s *AuthService) CreateUser(user models.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	//get user from DB
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 12).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		UserId: user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
